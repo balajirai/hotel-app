9112fb229d0c6965119ef8e8d59ee5156f40e461
Balaji <iam.balajirai@gmail.com>
2024-03-11 19:57:49 +0530

    search using t

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -1,10 +1,10 @@
 // SearchBar.jsx
-import React from 'react';
-import { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 
 function SearchBar({ data, onSearch }) {
 
   const [query, setQuery] = useState('');
+  const [isFocused, setIsFocused] = useState(false);
 
   const handleInputChange = (event) => {
     const newQuery = event.target.value;
@@ -12,17 +12,36 @@ function SearchBar({ data, onSearch }) {
     onSearch(newQuery);
   }
 
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 't' && !isFocused) {
+        event.preventDefault();
+        document.getElementById('searchInput').focus();
+      }
+    };
+
+    document.addEventListener('keypress', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keypress', handleKeyPress);
+    };
+  }, [isFocused]); // Now include isFocused in the dependency array
+
   return (
     <div className="flex flex-row">
       <input
+        id="searchInput"
         type="text"
         placeholder="Search for hotels..."
         className="ml-auto w-full px-3 py-1 rounded-md border border-gray-300 focus:outline-none focus:ring focus:ring-indigo-200 focus:border-0"
         value={query}
         onChange={handleInputChange}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)} 
       />
     </div>
   );
 }
 
 export default SearchBar;
+
